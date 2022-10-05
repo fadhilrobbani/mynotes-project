4809c5bf39af78df3318bd3b9728566b784d065a
fadhilrobbani <rabbanifadhillailham@gmail.com>
2022-10-05 21:16:25 +0700

    add controller notes

---
+++ server/src/controllers/NotesController.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from 'express';
+
+const findAllNotes = (req: Request, res: Response) => {};
+
+const findNote = (req: Request, res: Response) => {};
+
+const createNote = (req: Request, res: Response) => {};
+
+export { findAllNotes, createNote, findNote };
--- server/src/routes/notes.ts
+++ server/src/routes/notes.ts
@@ -1,3 +1,13 @@
 import express from 'express';
+import {
+  createNote,
+  findAllNotes,
+  findNote,
+} from '../controllers/NotesController';
+import notes from '../db/models/notes';
 
 const notesRouter = express.Router();
+
+notesRouter.get('/', findAllNotes);
+notesRouter.get('/notes/:id', findNote);
+notesRouter.post('/notes/new', createNote);
